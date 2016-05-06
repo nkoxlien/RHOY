b406c314d06652d44d191aff71768adc7a04d4dc
Nick <koxlienn@gmail.com>
2016-05-06 15:30:33 -0500

    Last push

--- main.cpp
+++ main.cpp
@@ -165,7 +165,7 @@ int socketStuff(float x, float y, float z){
     hints.ai_flags = 0;
     hints.ai_protocol = 0;
     
-    s = getaddrinfo("192.168.1.3", "22", &hints, &result);
+    s = getaddrinfo("192.168.1.2", "22", &hints, &result);
     
     if(s != 0){
 	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
@@ -242,45 +242,3 @@ int socketStuff(float x, float y, float z){
 
 }
 
-/*int connect(){
-
-
-    int sockfd;
-    struct addrinfo hints, *result, *rp;
-    int rv, s;
-    ssize_t nread;
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = 0;
-    hints.ai_protocol = 0;
-    
-    s = getaddrinfo("192.168.1.105", "20000", &hints, &result);
-    
-    if(s != 0){
-	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-        exit(EXIT_FAILURE);
-    }
-  
-    for(rp = result; rp != NULL; rp = rp->ai_next){
-	sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-	if(sockfd == -1)
-	    continue;
-	if(connect(sockfd, rp->ai_addr, rp->ai_addrlen) != -1){
-	    
-	    printf("Connected!!\n");
-	    //success
-	    break;
-	}
-	close(sockfd);
-    } 
-    
-    if(rp == NULL){
-
-	fprintf(stderr, "Couldn not connect\n");
-	exit(EXIT_FAILURE);
-    }
-
-    freeaddrinfo(result);
-}*/
